ff8a5fc96e05ab3ceb403ba09861f12dd095f8d7
JaeIn1 <ysy06053@gmail.com>
2023-12-22 10:11:37 +0900

    배포 오류 수정중

--- pages/projects.js
+++ pages/projects.js
@@ -28,7 +28,7 @@ export default function Projects({ projects }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const options = {
     method: "POST",
     headers: {
@@ -55,5 +55,9 @@ export async function getStaticProps() {
 
   const projects = await res.json();
 
-  return { props: { projects }, revalidate: 1 };
+  return {
+    props: { projects }, // will be passed to the page component as props
+    // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
+    // revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
+  };
 }
