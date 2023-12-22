e6e2f335571450a4a5b8517630c1958391aac490
JaeIn1 <ysy06053@gmail.com>
2023-12-22 10:31:56 +0900

    배포 오류 수정중

--- next.config.js
+++ next.config.js
@@ -8,6 +8,17 @@ const nextConfig = {
       "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
   },
+  source: "/api/:path*",
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+    {
+      key: "Access-Control-Allow-Headers",
+      value:
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    },
+  ],
 };
 
 module.exports = nextConfig;
